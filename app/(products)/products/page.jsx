58a8e723d130e2128dfc304815d8ef12dc2c9808
Salman-165324 <salmansadi165324@gmail.com>
2025-05-01 19:44:51 +0600

    refactor: change redirect from dashboard to login and remove ProductsPage component

--- app/(products)/products/page.jsx
+++ app/(products)/products/page.jsx
@@ -1,14 +1,13 @@
-'use client';
+"use client";
 
-import AdminLayout from '@/components/layout/AdminLayout';
-import ProductList from '@/components/product/ProductList';
+import AdminLayout from "@/components/layout/AdminLayout";
+import ProductList from "@/components/product/ProductList";
 
 export default function ProductsPage() {
   return (
     <AdminLayout>
       <div className="p-0 md:p-2">
         <ProductList />
-      
       </div>
     </AdminLayout>
   );
